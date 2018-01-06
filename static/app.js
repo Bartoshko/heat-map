85f9d279675d9ebfb5b77f7f7f6042268cfa2809
Bartoshko <lenart.consulting@gmail.com>
2018-01-06 17:02:07 +0100

    separation in x and y direction

--- static/app.js
+++ static/app.js
@@ -1,3 +1,13 @@
-const heatmap = new Heatmap(1000, 1000, 10, {left: 30, right: 10, top: 30, bottom: 10}, '#ff0000', '#ffffff');
-heatmap.toggleMouseEvents = false;
+const data = [
+	{x: 10, y: 10},
+	{x: 20, y: 20},
+	{x: 30, y: 30},
+	{x: 40, y: 40},
+	{x: 50, y: 50},
+	{x: 60, y: 60}
+];
+
+const heatmap = new Heatmap(120, 120, 10, {left: 20, right: 0, top: 20, bottom: 0}, '#ff0000', '#ffffff');
+heatmap.toggleMouseEvents = true;
 heatmap.create('chart');
+heatmap.feedWithCoordinates = data;
--- static/heatmap.js
+++ static/heatmap.js
@@ -1,5 +1,5 @@
 class Heatmap {
-	constructor(width = 200, height = 200, hexSize = 5, margin = {left: 10, right: 10, top: 10, bottom: 10}, heatColor = '#ff0000') {
+	constructor (width = 200, height = 200, hexSize = 5, margin = {left: 10, right: 10, top: 10, bottom: 10}, heatColor = '#ff0000') {
 		this._width = width;
 		this._height = height;
 		this._hexRadius = hexSize;
@@ -10,6 +10,10 @@ class Heatmap {
 			console.error(err);
 			this._hexRadius = this._width * this._height / 100000;
 		}
+		this._strokeWidth = 1;
+
+		this._hexSeparationX = Math.sqrt(3) * this._hexRadius + 2 * this._strokeWidth;
+		this._hexSeparationY = this._hexRadius + 2 * this._strokeWidth;
 
 		try {
 			if (!Number.isInteger(margin.left)) throw 'margin length wrongly specified';
@@ -40,6 +44,66 @@ class Heatmap {
 
 		// delete after tests
 		this._info();
+	}
+
+	create (id) {
+		this._svg = d3.select(`#${id}`).append("svg")
+			.attr("width", this._width + this._margin.left + this._margin.right)
+			.attr("height", this._height + this._margin.top + this._margin.bottom)
+			.append("g")
+			.attr("transform", "translate(" + this._margin.left + "," + this._margin.top + ")");
+
+		const hexbin = d3.hexbin().radius(this._hexRadius);
+		const points = this._points;
+		const strokeColor = this._heatColor;
+		const mouseEvents = this._alowMouseEvents;
+		const heatingTime = this._heatingUpTime;
+		const coolingTime = this._coolingDownTime;
+		const strokeWidth = this._strokeWidth;
+
+		function heatUp (d) {
+			if(mouseEvents) {
+			  const el = d3.select(this)
+			    .transition()
+			    .duration(heatingTime)
+			    .style("fill-opacity", .3)
+			    .style("stroke-opacity", .3);
+			}
+		}
+
+		function coolDown (d) {
+			if(mouseEvents) {
+			  const el = d3.select(this)
+			     .transition()
+			     .duration(coolingTime)
+			     .style("fill-opacity", 0)
+			     .style("stroke-opacity", 0);
+			}
+		};
+
+		this._hexMap = this._svg.append("g")
+		  .selectAll(".hexagon")
+		  .data(hexbin(points))
+		  .enter().append("path")
+		  .attr("class", "hexagon")
+		  .attr("d", function (d) {
+		  return "M" + d.x + "," + d.y + hexbin.hexagon();
+			})
+		  .attr("stroke", function (d,i) {
+		  	return strokeColor;
+			})
+			.style("stroke-opacity", 1)
+		  .attr("stroke-width", `${strokeWidth}px`)
+		  .style("fill", function (d,i) {
+		  	return points[i][2];
+			})
+			.style("fill-opacity", 0)
+			.on("mouseover", heatUp)
+  		.on("mouseout", coolDown);
+
+  		console.log(this._points.length, this._hexMap[0].length);
+  		console.log(this._hexSeparationX + 2, this._hexRadius * 2 + 2);
+  		console.log(this._hexMap[0][29]);
 	}
 
 	set toggleMouseEvents (value) {
@@ -81,87 +145,35 @@ class Heatmap {
 		}
 	}
 
-	create (id) {
-		this._svg = d3.select(`#${id}`).append("svg")
-			.attr("width", this._width + this._margin.left + this._margin.right)
-			.attr("height", this._height + this._margin.top + this._margin.bottom)
-			.append("g")
-			.attr("transform", "translate(" + this._margin.left + "," + this._margin.top + ")");
+	set feedWithCoordinates (data) {
+		// todo: figure out how to relate points with hexMap based on hexSize to target correct point by coordinates finds points index and set corresponding hexMap hexPoint heat.
 
-		const hexbin = d3.hexbin().radius(this._hexRadius);
-		const points = this._points;
-		const strokeColor = this._heatColor;
-		const mouseEvents = this._alowMouseEvents;
 		const heatingTime = this._heatingUpTime;
 		const coolingTime = this._coolingDownTime;
 
-		function heatUp (d) {
-			if(mouseEvents) {
-			  const el = d3.select(this)
-			    .transition()
-			    .duration(heatingTime)
-			    .style("fill-opacity", .3)
-			    .style("stroke-opacity", .3);
-			}
+		const fireHeatAtLocation = index =>
+		{
+			d3.select(this._hexMap[0][index])
+	    	.transition()
+	    		.duration(heatingTime)
+	    		.style("fill-opacity", .3)
+	    		.style("stroke-opacity", .3)
+	    	.transition()
+	     		.duration(coolingTime)
+	     		.style("fill-opacity", 0)
+	     		.style("stroke-opacity", 0);
+	   }
+
+	   if (Array.isArray(data)) {
+			data.forEach(arg => console.log(arg));
+		} else {
+			console.log(data);
 		}
 
-		function coolDown (d) {
-			if(mouseEvents) {
-			  const el = d3.select(this)
-			     .transition()
-			     .duration(coolingTime)
-			     .style("fill-opacity", 0)
-			     .style("stroke-opacity", 0);
-			}
-		};
+		console.log(this._hexMap.size());
+		// fireHeatAtLocation(28);
+		// fireHeatAtLocation(29);
 
-		const hexMap = this._svg.append("g")
-		  .selectAll(".hexagon")
-		  .data(hexbin(points))
-		  .enter().append("path")
-		  .attr("class", "hexagon")
-		  .attr("d", function (d) {
-		  return "M" + d.x + "," + d.y + hexbin.hexagon();
-			})
-		  .attr("stroke", function (d,i) {
-		  	return strokeColor;
-			})
-			.style("stroke-opacity", 0)
-		  .attr("stroke-width", "1px")
-		  .style("fill", function (d,i) {
-		  	return points[i][2];
-			})
-			.style("fill-opacity", 0)
-			.on("mouseover", heatUp)
-  		.on("mouseout", coolDown);
-
-  		// todo: figure out how to relate points with hexMap based on hexSize to target correct point by coordinates finds points index and set corresponding hexMap hexPoint heat.
-  		const fireHeatAtLocation = index =>
-  		{
-  			d3.select(hexMap[0][index])
-		    	.transition()
-		    		.duration(heatingTime)
-		    		.style("fill-opacity", .3)
-		    		.style("stroke-opacity", .3)
-		    	.transition()
-		     		.duration(coolingTime)
-		     		.style("fill-opacity", 0)
-		     		.style("stroke-opacity", 0);
-		   };
-
-  		console.log(hexMap[0][22]);
-
-  		let counter = 0;
-
-  		const looper = () => {
-  			if(counter < hexMap[0].length) {
-	  			setTimeout(() => {
-	  				fireHeatAtLocation(counter++);
-	  				looper();
-	  			},100);
-  			}
-  		};
-  		// looper();
 	}
 
 	_info () {
@@ -175,9 +187,9 @@ class Heatmap {
 	}
 
 	_calculateHexCentersArr () {
-		for (var i = 0; i < this._calculateMap()[1]; i++) {
-    	for (var j = 0; j < this._calculateMap()[0]; j++) {
-      	this._points.push([this._hexRadius * j * 1.5, this._hexRadius * i * 1.5, this._heatColor]);
+		for (let i = 0; i < this._calculateMap()[1]; i++) {
+    	for (let j = 0; j < this._calculateMap()[0]; j++) {
+      	this._points.push([this._hexRadius * j *1.5, this._hexRadius * i * 1.5, this._heatColor]);
     	}
 		}
 	}
